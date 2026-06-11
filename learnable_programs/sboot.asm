2d997b8a06b8f2c5e50531176ee7ea9ade7560d8
Soulfrya <70708786+craterworldstudio@users.noreply.github.com>
2026-06-11 06:20:29 +0530

    Refactor bootloader to increment character output

--- learnable_programs/sboot.asm
+++ learnable_programs/sboot.asm
@@ -1,11 +1,15 @@
 bits 16
 org 0x7C00
 
-mov ah, 0x0E
-mov al, 'A'
-int 0x10
 
-jmp $
+start: 
+  mov ah, 0x0E
+  mov al, 'A'
+  int 0x10
+
+  add al, 1
+
+  jmp start
 
 times 510-($-$$) db 0
 dw 0xAA55
