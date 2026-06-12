94f4314334ba95b2a72ce611d06cb67b356c71d7
tori <70708786+craterworldstudio@users.noreply.github.com>
2026-06-12 15:08:55 +0530

    made print_str, print_num, print_nl.

--- src/main.asm
+++ src/main.asm
@@ -1,17 +1,27 @@
 bits 16
 org 0x7C00
 
+; Setup SECTOR --------------------------------------------
+cli
+
+xor ax, ax      ; AX = 0
+mov ds, ax      ; for memory DS:offset
+mov ss, ax      ; for stack base/segment SS:SP
+mov sp, 0x7C00  ; for stack offset/pointer
+
+sti
+
 jmp main
 
 ; Data SECTOR ---------------------------------------------
 
 msgb db "Booted!", 0
 msgh db "Halted-", 0
-msge db "EoC.", 0
+msgcl db "EoC. Current length: ", 0
 
 section .text
 
-print:
+print_str:
     mov ah, 0x0E
     mov si, 0
     .loop:
@@ -29,7 +39,6 @@ print:
         ret
 
 print_nl:
-    call print
 
     mov ah, 0x0E
     mov al, 0x0D                        ; CR
@@ -43,8 +52,46 @@ print_nl:
 
     ret
 
+print_num:
+
+    push bx
+    mov bx, 10
+    push CX
+    mov cx, 0
+
+    .divide_loop:
+        xor dx, dx
+
+        div bx ;Division happens at DX:AX / BX and Q = AX and R = DX
+        push dx
+
+        add cx, 1
+
+        cmp ax, 0
+        jne .divide_loop
+        je .print_loop
+        
+
+    .print_loop:
+        pop ax
+        mov ah, 0x0E
+        add al, '0'
+        int 0x10
+
+        sub cx, 1
+        cmp cx, 0
+        je .done
+
+        jmp .print_loop
+
+    .done:
+        pop cx
+        pop bx
+        ret
+
 main:
     mov bx, msgb
+    call print_str
     call print_nl
 
     jmp halt
@@ -54,8 +101,10 @@ halt:
 
 
     mov bx, msgh
+    call print_str
     call print_nl
 
+    dw 0xFF
 
 
 
@@ -69,8 +118,14 @@ halt:
 
 
 
-    mov bx, msge
-    call print
+    mov bx, msgcl
+    call print_str
+    ;call print_nl
+
+    mov ax, (p_end - $$)
+    ;mov ax, 145
+    call print_num
+
     jmp .end
 
     .end:
@@ -79,7 +134,7 @@ halt:
         hlt
         jmp .end
 
-
+p_end: ;
 
 times 510-($-$$) db 0
 dw 0xAA55
